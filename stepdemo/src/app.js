617d4e72c3b6c25ffd358e28d3b6a692f10ffed9
Sumitprajapatintv <sumit963605@gmail.com>
2024-05-26 21:10:11 +0530

    step component

---
+++ stepdemo/src/app.js
@@ -0,0 +1,29 @@
+import "./app.css";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+function App() {
+  const step = 1;
+  return (
+    <div className="container">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button className="btn">Previous</button>
+        <button className="btn">Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
