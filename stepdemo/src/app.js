cfab668017eebf09835c402be3783eb8a23155a2
Sumitprajapatintv <sumit963605@gmail.com>
2024-06-18 08:56:22 +0530

    Learning State

--- stepdemo/src/app.js
+++ stepdemo/src/app.js
@@ -7,6 +7,8 @@ const messages = [
 ];
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setisOpen] = useState(true);
+  // const [test, setTest] = useState({ name: "Jonas" });
   function handlePrevious() {
     if (step > 1) {
       setStep(step - 1);
@@ -15,29 +17,38 @@ function App() {
   function handleNext() {
     if (step < 3) {
       setStep(step + 1);
+      // setTest({ name: "freed" });
     }
   }
   return (
-    <div className="container">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setisOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="container">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
-      <div className="buttons">
-        <button className="btn" onClick={handlePrevious}>
-          Previous
-        </button>
-        <button className="btn" onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            <button className="btn" onClick={handlePrevious}>
+              Previous
+            </button>
+            <button className="btn" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
