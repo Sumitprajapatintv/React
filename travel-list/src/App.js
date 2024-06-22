3584b88ceab9c8e4737765b3018ac9b9343b92c1
Sumitprajapatintv <sumit963605@gmail.com>
2024-06-22 21:57:15 +0530

    Rendering List

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 4, description: "Changer", quantity: 1, packed: true },
+  { id: 5, description: "Socks", quantity: 12, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -21,7 +27,27 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <li>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </li>
+    </div>
+  );
+}
+function Item({ item }) {
+  // console.log("item", item);
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
