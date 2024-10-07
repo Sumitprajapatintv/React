7e671ea16e459d873dd3d20b6938b6983e8d573d
sumitprydotintuit <abc@example.com>
2024-10-07 14:25:23 +0530

    use Reducer in React Quiz App

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,31 @@
 import './index.css'
 import Main from './Main';
 import Header from './Header'
+import { useEffect, useReducer } from 'react';
+const intialState = {
+  questions: [],
+
+  status: "Loding"
+}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" }
+    case "dataFailed":
+      return { ...state, status: "ready" }
+    default:
+      throw new Error("Action Unknown")
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions").then((res) => res.json()).
+      then((data) => dispatch({ type: "dataRecived", payload: data })).
+      catch((err) => dispatch({ type: "dataFailed" }))
+  })
   return (
     <div className="app">
       <Header />
