bc5b3c236cbc07b87a7292f63677439790ba34bc
sumitprydotintuit <abc@example.com>
2024-10-09 17:54:55 +0530

    useReducer learning

--- react-quiz/src/Components/NextQuestion.js
+++ react-quiz/src/Components/NextQuestion.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export default function NextQuestion({ dispatch, answer }) {
+export default function NextQuestion({ index, noOfQuestion, dispatch, answer }) {
   if (answer === null) return;
-  return (
-    <div>
-      <button className='btn btn-ui' onClick={() => dispatch({ type: "nextQuestion" })}>Next</button>
-    </div>
-  )
+  if (index < noOfQuestion - 1) {
+    return (
+      <div>
+        <button className='btn btn-ui' onClick={() => dispatch({ type: "nextQuestion" })}>Next</button>
+      </div>
+    )
+  }
+  if (index == noOfQuestion - 1) {
+    return (
+      <div>
+        <button className='btn btn-ui' onClick={() => dispatch({ type: "finished" })} >Finish</button>
+      </div>
+    )
+  }
+
 }
